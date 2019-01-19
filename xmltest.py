ebe4ad8a07ac3b6aad7ece69f0ab3662ab65d43d
ZJT1024 <45893596+ZJT1024@users.noreply.github.com>
2019-01-19 21:26:28 +0800

    增加实时显示两点最短路、全局最短路功能

--- xmltest.py
+++ xmltest.py
@@ -1,210 +1,108 @@
 import xml.etree.cElementTree as ET
-import subwayline
-import subwaymap
 import subwaystation
 import tkinter as tk
 import codecs
 from tkinter import messagebox
 import time
 
-
-
-tree = ET.parse('stations_v5.xml')
-map = tree.getroot()
-# 长宽记录
-w = int(map.find('w').text)
-h = int(map.find('h').text)
-station_line_cnt = {}
-transfer_station_list = []
-linecnt = 0
-# 遍历路线
-station_list = []
+# 全局变量
+# 线路树，储存线路是否是环线、车站名字、换乘车站名字、颜色
 line_dic = {}
-loop_line = ['Line 2', 'Line 4', "Line 10"]
-special_cnt = 0
-station_sname_lname_list = {}
-for line in map.findall('line'):
-    # 标记北京线路的个数
-    linecnt += 1
-    # 遍历车站
-    lname = line.find('lname').text
-    color = line.find('color').text
-    print(color)
-    line_dic[lname] = {}
-    station_cnt = 0
-    is_loop = False
-    if lname == 'Line 2' or lname == 'Line 4' or lname == 'Line 10':
-        is_loop = True
-    line_dic[lname]['is_loop'] = is_loop
-    line_dic[lname]['transfer_station_name_list'] = []
-    line_dic[lname]['station_name_list'] = []
-    color = str(color)
-    color = '#' + color.upper()
-    line_dic[lname]['color'] = color
-
-    for station in line:
-        # 标记为是否为环线
-        if station.tag != 'station':
-            continue
-        # 具体的结果
-        station_cnt += 1
-        staid = station.find('staid').text
-        name = station.find('name').text
-        sname = station.find('sname').text
-        sname = str(sname)
-        sname = sname.strip().split()
-        sname = sname[len(sname)-1]
-        mapx = float(station.find('mapx').text)/2
-        mapy = float(station.find('mapy').text)/2
-        pos = int(station.find('pos').text)
-        lat = float(station.find('lat').text)
-        lng = float(station.find('lng').text)
-        # 如果station_line_cnt中存在sname则说明此站肯定是换乘站
-        print(lname, staid, name, sname, mapx, mapy, pos, lat, lng)
-
-        # 找一个西直门验证想法，会不会牵一发而动全身？
-        is_transfer = False
-        for tmp_station in station_list:
-            if tmp_station.sname == sname:
-                is_transfer = True
-                tmp_station.add_lname(lname)
-                transfer_station_list.append(tmp_station)
-                break
-        if not is_transfer:
-            tmp_station = subwaystation.station(staid, name, sname, mapx, mapy, pos, lat, lng, lname, station_cnt)
-            station_list.append(tmp_station)
-        line_dic[lname]['station_name_list'].append(sname)
-
-print()
-for tmp_station in station_list:
-    if len(tmp_station.lname_list) > 1:
-        transfer_station_list.append(tmp_station)
-
-print()
-
-for tmp_station in transfer_station_list:
-    tmp_lname_list = tmp_station.lname_list
-    sname = tmp_station.sname
-    for tmp_lname in tmp_lname_list:
-        if sname not in line_dic[tmp_lname]['transfer_station_name_list']:
-            line_dic[tmp_lname]['transfer_station_name_list'].append(sname)
-        else:
-            continue
-
-for tmp_station in station_list:
-    station_sname_lname_list[tmp_station.sname] = tmp_station.lname_list
-
-lname_jud = {}
-for key in line_dic:
-    lname_jud[key] = False
-
-
-# 传的话，只传line_dic即可
-# 站的信息也得传！有站的信息才能绘图
+# 站点链表，其中每个结点储存站点的所有信息：包括坐标、名字、所属的路线等
+station_list = []
+# 换乘车站链表，其中每个结点储存换成车站的所有信息：包括坐标、名字、所属的路线
+transfer_station_list = []
+# 车站和UI中的标签对应的链表，其中每个元素组成为（车站，标签）
 station_label_list = []
 
-# 要包含起点站的名字，终点站的名字，现在站的名字，当前花费，最小花费，当前线路
-# 当前最短路径
-ans_sname_path = []
-# 最短路径的集合
-ans_sname_path_list = []
-# line_dic存放所有的信息
-print("---------------------")
-for station in transfer_station_list:
-    print(station.sname)
-print("---------------------")
+def decode_xml(path):
+    tree = ET.parse(path)
+    map = tree.getroot()
+    # 长宽记录
+    w = int(map.find('w').text)
+    h = int(map.find('h').text)
+    linecnt = 0
+    # 遍历路线
+    loop_line = ['Line 2', 'Line 4', "Line 10"]
+    special_cnt = 0
+    for line in map.findall('line'):
+        # 标记北京线路的个数
+        linecnt += 1
+        # 遍历车站
+        lname = line.find('lname').text
+        color = line.find('color').text
+        # print(color)
+        line_dic[lname] = {}
+        station_cnt = 0
+        is_loop = False
+        if lname == 'Line 2' or lname == 'Line 4' or lname == 'Line 10':
+            is_loop = True
+        line_dic[lname]['is_loop'] = is_loop
+        line_dic[lname]['transfer_station_name_list'] = []
+        line_dic[lname]['station_name_list'] = []
+        color = str(color)
+        color = '#' + color.upper()
+        line_dic[lname]['color'] = color
 
+        for station in line:
+            # 标记为是否为环线
+            if station.tag != 'station':
+                continue
+            # 具体的结果
+            station_cnt += 1
+            staid = station.find('staid').text
+            name = station.find('name').text
+            sname = station.find('sname').text
+            sname = str(sname)
+            sname = sname.strip().split()
+            sname = sname[len(sname) - 1]
+            # 等比例缩小坐标
+            mapx = float(station.find('mapx').text) / 2
+            mapy = float(station.find('mapy').text) / 2
+            pos = int(station.find('pos').text)
+            lat = float(station.find('lat').text)
+            lng = float(station.find('lng').text)
+            # 如果station_line_cnt中存在sname则说明此站肯定是换乘站
+            # print(lname, staid, name, sname, mapx, mapy, pos, lat, lng)
 
-# 寻找路径
-def search(start_name, end_name, now_name,cost,min_cost):
-    is_same_line = False
-    # 看看是否在同一条线上
-    lname = ''
-    for lname1 in station_sname_lname_list[now_name]:
-        for lname2 in station_sname_lname_list[end_name]:
-            if lname1 == lname2:
-                lname = lname1
-                is_same_line = True
+            is_transfer = False
+            for tmp_station in station_list:
+                if tmp_station.sname == sname:
+                    is_transfer = True
+                    tmp_station.add_lname(lname)
+                    transfer_station_list.append(tmp_station)
+                    break
+            if not is_transfer:
+                tmp_station = subwaystation.station(staid, name, sname, mapx, mapy, pos, lat, lng, lname, station_cnt)
+                station_list.append(tmp_station)
+            line_dic[lname]['station_name_list'].append(sname)
 
-    # tmp_cost 是从当前车站到终点站或下一个车站的集合
-    tmp_cost = 0
-    # dir 标记正向还是负向，如果dir = True则为正向，否则为负向
-    dir = True
-    if is_same_line:
-        pos1 = 0
-        pos2 = 0
-        # 如果现在的站和终点站在一条路线上
-        # 标记一下正向和负向
-        # 默认标记为正向
-        for i in range(0, len(line_dic[lname]['station_name_list'])):
-            if line_dic[lname]['station_name_list'][i] == now_name:
-                pos1 = i
-            if line_dic[lname]['station_name_list'][i] == end_name:
-                pos2 = i
-        if pos1 > pos2:
-            flag = False
-        # 如果是环线
-        if line_dic[lname]['is_loop']:
-            # 从开始到结束
-            tmp_cost1 = abs(pos2-pos1)+1
-            tmp_cost2 = len(line_dic[lname]['station_name_list'])-abs(pos2-pos1)+1
-            if tmp_cost1 < tmp_cost2:
-                tmp_cost = tmp_cost1
-                if flag:
-                    dir = True
-                else:
-                    dir = False
+    print()
+    for tmp_station in station_list:
+        if len(tmp_station.lname_list) > 1:
+            transfer_station_list.append(tmp_station)
+
+    print()
+
+    for tmp_station in transfer_station_list:
+        tmp_lname_list = tmp_station.lname_list
+        sname = tmp_station.sname
+        for tmp_lname in tmp_lname_list:
+            if sname not in line_dic[tmp_lname]['transfer_station_name_list']:
+                line_dic[tmp_lname]['transfer_station_name_list'].append(sname)
             else:
-                tmp_cost = tmp_cost2
-                if flag:
-                    dir = False
-                else:
-                    dir = True
-        else:
-            tmp_cost = abs(pos2-pos1)+1
-            if flag:
-                dir = True
-            else:
-                dir = False
-        if tmp_cost + cost < min_cost:
-            cost += tmp_cost
+                continue
+    return
+xml_path = 'stations_v5.xml'
+decode_xml(xml_path)
 
-
-    else:
-        pass
-
-subway_info = "subway_info.txt"
-line_info = "line_info.txt"
-subway_text = codecs.open(subway_info, 'w', 'utf-8')
-for station in station_list:
-    subway_text.write(station.sname + "\n")
-    for lname in station.lname_list:
-        subway_text.write(lname + "\\")
-    subway_text.write("\n")
-    subway_text.write("\n")
-subway_text.close()
-
-line_text = codecs.open(line_info, 'w', 'utf-8')
-for key in line_dic:
-    print(key)
-    line_text.write(key + "\n")
-    line_text.write(str(line_dic[key]['is_loop'])+"\n")
-    for sname in line_dic[key]['transfer_station_name_list']:
-        line_text.write(sname)
-        line_text.write(' ')
-    line_text.write("\n")
-    for sname in line_dic[key]['station_name_list']:
-        line_text.write(sname)
-        line_text.write(" ")
-    line_text.write("\n")
-    # 最后一个需要换行
-    line_text.write("\n")
-
-line_text.close()
-
+# UI部分，包括UI部分的所有插件
+# 缺少Tkinter下MVC的配置资料，故解析和UI有些杂糅
 window = tk.Tk()
 window.title("subway MAP UI")
-window.geometry('1000x900')
+window.geometry('1200x1000')
+
+# 寻找最短路线的排版
 e1 = tk.Entry(window)
 e2 = tk.Entry(window)
 l1 = tk.Label(window, text="起始站", font=('Aerial', 15))
@@ -213,12 +111,10 @@ l2 = tk.Label(window, text="终点站",font=('Aerial', 15))
 l2.place(x=0, y = 30)
 e1.place(x=50,y = 0)
 e2.place(x=50,y = 30)
-
-
-# UI界面应该和这些界面分开来放
-name_path = []
-
-def find_a_way():
+# 根据题目要求查找从某个起点车站到某个终点车站的路线
+def find_a_way_by_cost():
+    # 按站寻路为0
+    flag = 0
     for (station, l) in station_label_list:
         lname_list = station.lname_list
         if len(lname_list) > 1:
@@ -241,32 +137,60 @@ def find_a_way():
         return
     ###########
 
-    print(start_name, end_name)
-    name_list = []
-    name_list.append(start_name)
-    name_list.append(end_name)
-    # path_name_list:
-    for sname in name_list:
+
+
+    ##########
+
+    # python 调用C++
+    ##########
+
+    ##########
+    # python对文件进行处理
+    tmp_sname_list = []
+    input_path = 'station_order.txt'
+    input_file = codecs.open(input_path, 'r', 'utf-8')
+    i = 0
+    output = ''
+    for line in input_file.readlines():
+        output += line
+        if i == 0:
+            i += 1
+            continue
+        info = line.strip().split()
+        tmp_sname_list.append(info[0])
+
+    # 动态显示线路
+    for tmp_sname in tmp_sname_list:
         for (tmp_station, l) in station_label_list:
-            if tmp_station.sname == sname:
-                l['bg'] = 'green'
+            if tmp_sname == tmp_station.sname:
+                l['bg'] = 'white'
+                time.sleep(.200)
+                l.update()
                 break
 
-
-
-button = tk.Button(window, text="寻找最短路径", bg='white', width=15, height=2, command=find_a_way)
+    # 更改右侧的结果
+    T.delete(0.0,tk.END)
+    T.insert('insert', output)
+button = tk.Button(window, text="按站寻路", bg='white', width=10, height=2, command=find_a_way_by_cost)
 button.place(x=0, y=60)
-e3 = tk.Entry(window)
-e3.place(x=0, y=130)
+# 后期得更改button1的函数
+def find_a_way_by_effort():
+    pass
+button1 = tk.Button(window, text="按精力寻路", bg='white', width=10, height=2, command=find_a_way_by_effort)
+button1.place(x=120, y=60)
 
-
+# 右侧文本框的排版
 T = tk.Text(window, height=50, width=30)
 T.place(x=1000, y=0)
 T.insert('insert',"你好！")
+
+# 按照线路名寻找的排版
+e3 = tk.Entry(window)
+e3.place(x=0, y=130)
 l3 = tk.Label(window, text="输入线路名",font=('Aerial', 15))
 l3.place(x=0, y = 100)
 
-
+# 根据题目要求，输入线路名字，可动态显示此站点中所有车站，在UI界面上显示，
 def show_line():
     # 颜色复原
     for (station, l) in station_label_list:
@@ -283,11 +207,7 @@ def show_line():
         return
 
     # 显示颜色
-    flag = []
-    for i in range(0,len(station_label_list)):
-        flag.append(False)
     tmp_sname_list = []
-    print("----------------------------------------")
     for sname in line_dic[line_name]['station_name_list']:
         tmp_sname_list.append(sname)
     output = ''
@@ -305,10 +225,6 @@ def show_line():
                 l['bg'] = 'white'
                 time.sleep(.300)
                 l.update()
-
-
-    print("----------------------------------------")
-
     # 颜色复原
     time.sleep(3)
     for (station, l) in station_label_list:
@@ -322,11 +238,10 @@ def show_line():
     for (station, l) in station_label_list:
         l.update()
     return
+button2 = tk.Button(window, text="显示路线", bg='white', width=15, height=2, command=show_line)
+button2.place(x=0, y=160)
 
-button1 = tk.Button(window, text="显示路线", bg='white', width=15, height=2, command=show_line)
-button1.place(x=0, y=160)
-
-
+# 创建标签，其中每一个标签都是地铁上的一个站
 for tmp_station in station_list:
     text = tmp_station.sname
     mapx = tmp_station.mapx
@@ -342,7 +257,5 @@ for tmp_station in station_list:
     station_label_list.append((tmp_station, l))
 
 
-
+# 窗口的循环
 window.mainloop()
-
-
